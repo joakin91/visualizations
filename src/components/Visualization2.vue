41a7e76c195eff8783399130fc532a9852c7382a
Joaquin Treviño <joaquint1991@gmail.com>
2019-10-25 13:14:43 +0200

    Add lines that move randomly up and down.

--- src/components/Visualization2.vue
+++ src/components/Visualization2.vue
@@ -2,11 +2,16 @@
   <div id="container">
     <vgl-renderer antialias style="height: 100vh;">
         <vgl-scene>
-        <vgl-geometry name="vertices" position-attribute="-5, 0, 0, 10, 10, 0"></vgl-geometry>
+        <vgl-axes-helper size=140></vgl-axes-helper>
+        <!--position-attribute: The x, y, and z coordinates of each vertex in this geometry. Goes in triplets-->
+        <template v-for="line in lines">
+            <vgl-geometry :name="`${line.name}`" :position-attribute="`${line.vertices}`" v-bind:key="line.name"></vgl-geometry>
+            <vgl-line-loop :geometry="`${line.name}`" material="line" v-bind:key="line.name"></vgl-line-loop>
+        </template>
         <vgl-line-basic-material name="line" :linewidth="linewidth" :color="`#${[r, g, b].map(v => parseInt(v).toString(16)).map(v => v.length < 2 ? '0' + v : v).join('')}`"></vgl-line-basic-material>
-        <vgl-line-loop geometry="vertices" material="line"></vgl-line-loop>
         </vgl-scene>
-        <vgl-perspective-camera orbit-position="20 0.8 0.7"></vgl-perspective-camera>
+        <!--perspective camera I think it's in 2d already-->
+        <vgl-perspective-camera orbit-position="30 1 0"></vgl-perspective-camera>
     </vgl-renderer>
 
     <aside class="control-panel">
@@ -34,13 +39,47 @@ Object.keys(VueGL).forEach(name => {
 
 export default {
     name: 'Visualization2',
-    mounted: () => {},
+    mounted(){
+        setInterval(() => {
+            let indexOfLineToMove = Math.floor(Math.random() * this.$data.lines.length),
+                lineToMove = this.$data.lines[indexOfLineToMove];
+            let vertixToBaseChanges = lineToMove.vertices[1]
+            if( vertixToBaseChanges == 8 ){
+                lineToMove.increase = false
+            }else if( vertixToBaseChanges == -8 ){
+                lineToMove.increase = true
+            }
+            let newValue = vertixToBaseChanges + 1
+            if( !lineToMove.increase ){
+                newValue = vertixToBaseChanges - 1
+            }
+            Vue.set(lineToMove.vertices, 1, newValue)
+            Vue.set(lineToMove.vertices, 4, newValue)
+            // this.$date.lines.forEach((line, index, arr) => {
+                
+            // });
+        }, 50);
+    },
     data: () => {
         return {
             r: 255,
-            g: 255,
+            g: 255,  
             b: 255,
-            linewidth: 60,
+            linewidth: 2,
+            increase: true, 
+            vertices: [-10,0,0,5,0,0],
+            lines: [
+                {
+                    name: 'line1',
+                    increase: true, 
+                    vertices: [-15,0,0,5,0,0],   
+                },
+                {
+                    name: 'line2',
+                    increase: true, 
+                    vertices: [-10,0,0,5,0,0],   
+                },
+            ]
         }
     },
 }
